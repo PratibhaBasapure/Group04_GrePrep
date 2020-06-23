026485cb7667701f0f218bdd6f559e80f5b854f9
Padmesh Donthu <pd616769@dal.ca>
2020-06-23 02:18:46 -0300

    Changing server.js file

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Assignment2'));
+app.use(express.static(__dirname + '/dist/GrePrep'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/Assignment2/index.html'));
+res.sendFile(path.join(__dirname+'/dist/GrePrep/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
