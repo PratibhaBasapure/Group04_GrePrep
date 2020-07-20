711291cea4b05d09d6f1c5817321da6656ced000
Padmesh Donthu <pd616769@dal.ca>
2020-07-20 17:40:26 -0300

    Adding backend configurations for Web Project

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const mongoUrl =
+  "mongodb+srv://admin:dbadmin@greprep.ym7uf.mongodb.net/greprep?retryWrites=true&w=majority";
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: "false" }));
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+var app = require("./app");
+var http = require("http");
+var express = require("express");
+var path = require("path");
+const cors = require("cors");
+var port = process.env.PORT || "8080";
 
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/GrePrep'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/GrePrep/index.html'));
+app.use(express.static(__dirname + "/dist/GrePrep"));
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/GrePrep/index.html"));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+var server = http.createServer(app);
+server.listen(port);
