666cf282a20aa0cb86c3a4c8b5e2af6ec84a70d7
Neelesh <neelesh.homefilescenter14@gmail.com>
2020-07-25 20:15:25 +0530

    interface changes

--- src/app/gre-predictor/gre-predictor.component.ts
+++ src/app/gre-predictor/gre-predictor.component.ts
@@ -6,86 +6,9 @@ import { PredictionServiceService } from './prediction-service.service';
 import { UserService } from '../services/user.service';
 
 export interface GrePredictor {
-  country: string;
   school: string;
-  specialization: string;
   possibility: string;
 }
-
-const ELEMENT_DATA: GrePredictor[] = [
-  {
-    country: 'USA',
-    school: 'Northeastern University',
-    specialization: 'Masters in Data Science',
-    possibility: 'Dream',
-  },
-  {
-    country: 'USA',
-    school: 'Northeastern University',
-    specialization: 'Masters in Computer Science',
-    possibility: 'Reach',
-  },
-  {
-    country: 'USA',
-    school: 'Northeastern University',
-    specialization: 'Masters in Data Analytics',
-    possibility: 'Safe',
-  },
-  {
-    country: 'USA',
-    school: 'Northeastern University',
-    specialization: 'Masters in Data Science',
-    possibility: 'Reach',
-  },
-  {
-    country: 'USA',
-    school: 'Northeastern University',
-    specialization: 'Masters in Data Science',
-    possibility: 'Reach',
-  },
-  {
-    country: 'USA',
-    school: 'Arizona State University',
-    specialization: 'Masters in Computer Science',
-    possibility: 'Reach',
-  },
-  {
-    country: 'USA',
-    school: 'Arizona State University',
-    specialization: 'Masters in Electrical and Computer Engineering',
-    possibility: 'Safe',
-  },
-  {
-    country: 'USA',
-    school: 'University of South Carolina',
-    specialization: 'Masters in Computer Science',
-    possibility: 'Reach',
-  },
-  {
-    country: 'USA',
-    school: 'University of South Florida',
-    specialization: 'Master of Science in Computer Engineering',
-    possibility: 'Dream',
-  },
-  {
-    country: 'Singapore',
-    school: 'National University of Singapore',
-    specialization: 'Master in Computing',
-    possibility: 'Dream',
-  },
-  {
-    country: 'Canada',
-    school: 'University of Ontario Institute of Technology',
-    specialization: 'Master of Information Technology Security (MITS) ',
-    possibility: 'Reach',
-  },
-  {
-    country: 'USA',
-    school: 'San Jose State University',
-    specialization: 'Master of Science in Aerospace Engineering',
-    possibility: 'Safe',
-  },
-];
 
 @Component({
   selector: 'app-gre-predictor',
