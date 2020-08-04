b8013f4a1bfe2ea54b2c3f3667f13064f4f990c6
Padmesh Donthu <pd616769@dal.ca>
2020-08-04 11:06:28 -0300

    Refactoring footer component

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,8 +1,5 @@
 // Author - Padmesh Donthu
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { Router } from '@angular/router';
-import { MediaObserver, MediaChange } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-footer',
@@ -11,21 +8,6 @@ import { MediaObserver, MediaChange } from '@angular/flex-layout';
   encapsulation: ViewEncapsulation.Emulated,
 })
 export class FooterComponent implements OnInit {
-  mediaSubscribe: Subscription;
-  deviceXs: boolean;
-  constructor(private router: Router, private mediaObserver: MediaObserver) {}
-  ngOnDestroy(): void {
-    this.mediaSubscribe.unsubscribe();
-  }
-
-  ngOnInit(): void {
-    this.mediaSubscribe = this.mediaObserver.media$.subscribe(
-      (result: MediaChange) => {
-        this.deviceXs = result.mqAlias === 'xs' ? true : false;
-      }
-    );
-  }
-  openAttemptHistory() {
-    this.router.navigate(['attemptHistory']);
-  }
+  constructor() {}
+  ngOnInit(): void {}
 }
